0f41a99f50cc6417fcf94905f71ee35237a7da8a
Seys-sledgehammer <jonas.seys@noven.be>
2026-06-14 09:24:16 +0200

    Plattegrond

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Grensrock Draaiboek - network-first zodat de mobiele/raadpleeg-pagina automatisch de nieuwste versie toont */
-const CACHE = 'grensrock-draaiboek-v3';
-const ASSETS = ['./','./index.html','./editor.html','./mobiel.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE = 'grensrock-draaiboek-v4';
+const ASSETS = ['./','./index.html','./editor.html','./mobiel.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./plattegrond.jpg'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch', e => {
